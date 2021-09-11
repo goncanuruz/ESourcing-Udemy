33fd468add5dc0a6b808cb0031d6c643555955bc
goncanur uz <goncanuruz@outlook.com>
2021-09-11 15:51:29 +0300

    sourcing data settings folder

---
+++ Esourcing.Sourcing/Settings/ISourcingDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esourcing.Sourcing.Settings
+{
+    public interface ISourcingDatabaseSettings
+    {
+        public string ConnectionStrings { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
