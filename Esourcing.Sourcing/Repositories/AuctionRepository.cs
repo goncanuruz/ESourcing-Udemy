f66cd4e0a1807242f8a113b91423512575bdc7dc
goncanur uz <goncanuruz@outlook.com>
2021-09-11 16:19:47 +0300

    Auction Repository

---
+++ Esourcing.Sourcing/Repositories/AuctionRepository.cs
@@ -0,0 +1,55 @@
+﻿using Esourcing.Sourcing.Data.Interfaces;
+using Esourcing.Sourcing.Entities;
+using Esourcing.Sourcing.Repositories.Interfaces;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esourcing.Sourcing.Repositories
+{
+    public class AuctionRepository : IAuctionRepository
+    {
+        private readonly ISourcingContext _context;
+
+        public AuctionRepository(ISourcingContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(Auction auction)
+        {
+            await _context.Auctions.InsertOneAsync(auction);
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            FilterDefinition<Auction> filter = Builders<Auction>.Filter.Eq(x=>x.Id,id);
+            DeleteResult deleteResult = await _context.Auctions.DeleteOneAsync(filter);
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+        public async Task<Auction> GetAuction(string id)
+        {
+            return await _context.Auctions.Find(x => x.Id == id).FirstOrDefaultAsync() ;
+        }
+
+        public async Task<Auction> GetAuctionByName(string name)
+        {
+            FilterDefinition<Auction> filter = Builders<Auction>.Filter.Eq(x => x.Name, name);
+            return await _context.Auctions.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Auction>> GetAuctions()
+        {
+            return await _context.Auctions.Find(x=>true).ToListAsync();
+        }
+
+        public async Task<bool> Update(Auction auction)
+        {
+            var updatedResult = await _context.Auctions.ReplaceOneAsync(x=>x.Id.Equals(auction.Id),auction);
+            return updatedResult.IsAcknowledged && updatedResult.ModifiedCount > 0;
+        }
+    }
+}
